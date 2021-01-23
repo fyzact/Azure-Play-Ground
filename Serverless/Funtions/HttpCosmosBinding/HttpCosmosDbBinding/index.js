296c3e70da3eac06cbbe75081a6fe9a9e47b56ad
fyzact <feyyazacet@gmail.com>
2021-01-23 17:58:42 +0000

    add storage que storage to did not find cosmos item

--- Serverless/Funtions/HttpCosmosBinding/HttpCosmosDbBinding/index.js
+++ Serverless/Funtions/HttpCosmosBinding/HttpCosmosDbBinding/index.js
@@ -2,12 +2,17 @@ module.exports = async function (context, req,inputDocument) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if(!inputDocument){
-        let message="Todo item"+req.Query.id+"not found";
+        let message="Todo item"+req.query.id+"not found";
         context.log(message);
+        //if don find the  item in db  add que storage
+        
+        context.bindings.outputQueueItem=message;
         context.req={
-            // status:404,
+            status:404,
             body:message
         }
+
+        return;
     }
   
     context.res = {
