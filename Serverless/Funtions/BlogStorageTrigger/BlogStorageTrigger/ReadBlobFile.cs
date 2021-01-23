64023d164307b7caf245f883e1dd9a7de56dff4e
fyzact <feyyazacet@gmail.com>
2021-01-23 09:56:41 +0000

    Read csv file that upload blob

--- Serverless/Funtions/BlogStorageTrigger/BlogStorageTrigger/ReadBlobFile.cs
+++ Serverless/Funtions/BlogStorageTrigger/BlogStorageTrigger/ReadBlobFile.cs
@@ -11,7 +11,13 @@ namespace BlogStorageTrigger
         [FunctionName("ReadBlobFile")]
         public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "")] Stream myBlob, string name, ILogger log)
         {
-          
+            StreamReader streamReader = new StreamReader(myBlob);
+            string fileContent = streamReader.ReadToEnd();
+            var orderListCSV = fileContent.Split(Environment.NewLine.ToCharArray());
+            foreach (var item in orderListCSV)
+            {
+                Console.WriteLine(item);
+            }
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
     }
