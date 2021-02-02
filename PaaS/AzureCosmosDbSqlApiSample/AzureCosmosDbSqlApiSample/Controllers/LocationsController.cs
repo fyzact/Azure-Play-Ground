be94c94530fd8cf5051994f61d863f0c976f1c1c
fyzact <feyyazacet@gmail.com>
2021-02-02 18:42:23 +0000

    add locations get method to get location List

--- PaaS/AzureCosmosDbSqlApiSample/AzureCosmosDbSqlApiSample/Controllers/LocationsController.cs
+++ PaaS/AzureCosmosDbSqlApiSample/AzureCosmosDbSqlApiSample/Controllers/LocationsController.cs
@@ -34,5 +34,19 @@ namespace AzureCosmosDbSqlApiSample.Controllers
             var result = await locationContainer.CreateItemAsync(location, new PartitionKey(location.Country));
             return CreatedAtAction("Get", new { id = result.Resource.Id }, result.Resource);
         }
+
+        [HttpGet]
+        public async  Task<IActionResult> Get()
+        {
+            var iterator = locationContainer.GetItemQueryIterator<Location>() ;
+            List<Location> locations = new List<Location>();
+            while (iterator.HasMoreResults)
+            {
+                var pageOfLocation =await iterator.ReadNextAsync();
+                locations.AddRange(pageOfLocation.ToList());
+            }
+
+            return Ok(locations);
+        }
     }
 }
