98863886ab066e07d042fddabe801ff519548aac
fyzact <feyyazacet@gmail.com>
2021-02-05 02:25:58 +0000

    create project sample to use azure storage queue

---
+++ PaaS/AzureStorageQueueSample/AzureStorageQueueSample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureStorageQueueSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+
+            QueueClient queueClient = new QueueClient("connectionstring", "locations");
+            var serializeObject = JsonConvert.SerializeObject(new
+            {
+                country = "Uk",
+                Address = "Kingston",
+                Location = "London"
+            });
+            var byteArray = System.Text.Encoding.UTF8.GetBytes(serializeObject);
+            var response = queueClient.SendMessage(Convert.ToBase64String(byteArray));
+
+            return Ok("ok"); //Note: it shouldbe CreatedAtAction or Created
+        }
+    }
+}
