dc93909168e3ac157075109b0e03b2b4e92243c1
fyzact <feyyazacet@gmail.com>
2021-02-11 00:20:00 +0000

    add cosmosdb output binding

--- PaaS/AzureServiceBusSample/AzureServiceBusSample.Consumer/Function1.cs
+++ PaaS/AzureServiceBusSample/AzureServiceBusSample.Consumer/Function1.cs
@@ -1,13 +1,23 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace AzureServiceBusSample.Consumer
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("stoktransfer", Connection = "StocktransferQueue")]string myQueueItem, ILogger log)
+        public static void Run([ServiceBusTrigger("stoktransfer", Connection = "StocktransferQueue")]string myQueueItem,  [CosmosDB(databaseName:"tetriscdb",collectionName:"stock", ConnectionStringSetting= "stockDb")]out dynamic document,ILogger log)
         {
+            var dynamic = JsonConvert.DeserializeObject<dynamic>(myQueueItem);
+            document = new
+            {
+                id = System.Guid.NewGuid().ToString(),
+                category="stock",
+                productName=dynamic.Product,
+                quantity=dynamic.Quantity,
+                price=dynamic.Price
+            };
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
             
         }
